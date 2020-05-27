76c33a622a5185b96f9f97ce947436604de22f5a
Rajat <rajatarya199zx@gmail.com>
2020-05-27 17:02:45 +0530

    Add data to FireStore

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const cafeList =document.querySelector('#cafe-list');
-
+const form=document.querySelector('#add-cafe-form')
 // create element and render cafe
 function renderCafe(doc){
 
@@ -20,6 +20,7 @@ function renderCafe(doc){
     cafeList.appendChild(li);
 }
 
+//getting data
 db.collection('cafes').get().then(
     (snapshot)=>{
        snapshot.docs.forEach(doc=>{
@@ -28,3 +29,28 @@ db.collection('cafes').get().then(
        })
     }
 )  
+
+//saving data
+
+/*form.addEventListener('submit',(e)=>{
+    e.preventDefault(); // not going to refresh the page 
+
+    db.collection('cafes').add({
+        name:form.name.value,
+        city:form.city.value
+    })
+})
+
+
+it store into the firestore and also work without refresh */
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault(); // not going to refresh the page 
+
+    db.collection('cafes').add({
+        name:form.name.value,
+        city:form.city.value
+    })
+    form.name.value ='';
+    form.city.value='';
+})
